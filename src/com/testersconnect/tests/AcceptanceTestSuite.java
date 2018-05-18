871e84a9a9ad55521eba6255900ad4f3ba4c41b0
John Doe <john@sample.com>
2018-05-18 15:47:12 +0530

    First Commit.....

---
+++ src/com/testersconnect/tests/AcceptanceTestSuite.java
@@ -0,0 +1,20 @@
+package com.testersconnect.tests;
+
+import org.testng.annotations.Test;
+
+public class AcceptanceTestSuite {
+
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("Executing Test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Executing Test2");
+	}
+	
+	
+}
